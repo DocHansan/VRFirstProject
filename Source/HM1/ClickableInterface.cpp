53f8de8a640a76e6485ad08f28faf1b362871c80
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-23 00:50:12 +1000

    Add ClickableInterface

---
+++ Source/HM1/ClickableInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ClickableInterface.h"
+
+// Add default functionality here for any IClickableInterface functions that are not pure virtual.
