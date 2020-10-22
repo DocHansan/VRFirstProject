fcacedfc67255739d5bd86f350e4779171ed61b1
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-22 20:46:20 +1000

    Add ClickableActor

---
+++ Source/HM1/ClickableActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseActor.h"
+#include "ClickableActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HM1_API AClickableActor : public ABaseActor
+{
+	GENERATED_BODY()
+	
+};
