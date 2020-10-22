51229d6f03dd5e8b70df1ef37383bfb5fc186584
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-23 01:29:33 +1000

    Add ClickableInterface in ClickableActor

--- Source/HM1/ClickableActor.h
+++ Source/HM1/ClickableActor.h
@@ -4,13 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "BaseActor.h"
+#include "ClickableInterface.h"
 #include "ClickableActor.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class HM1_API AClickableActor : public ABaseActor
+class HM1_API AClickableActor : public ABaseActor, public IClickableInterface
 {
 	GENERATED_BODY()
 	
