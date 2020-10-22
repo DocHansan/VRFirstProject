3030fb67b77e618034fcaaee710018c93e3caf32
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-22 20:45:57 +1000

    Add PickableActor

---
+++ Source/HM1/PickableActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseActor.h"
+#include "PickableActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HM1_API APickableActor : public ABaseActor
+{
+	GENERATED_BODY()
+	
+};
