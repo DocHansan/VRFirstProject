17373e2ea48972127015f4fb829436f41b0f9fa2
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-22 22:29:48 +1000

    Add Variable for rendering Teleportation Trajectory

--- Source/HM1/BaseVRPawn.h
+++ Source/HM1/BaseVRPawn.h
@@ -19,6 +19,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = TeleportVisualizing)
+	bool AfterPressingTheButton;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
