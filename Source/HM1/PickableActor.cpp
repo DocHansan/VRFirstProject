8e310e860ddb0107611cfcd591b4dd5fd4d2c47b
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-23 19:19:06 +1000

    Add ClickableInterface in PickableActor

--- Source/HM1/PickableActor.cpp
+++ Source/HM1/PickableActor.cpp
@@ -3,3 +3,25 @@
 
 #include "PickableActor.h"
 
+APickableActor::APickableActor()
+{
+    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void APickableActor::Click_Implementation()
+{
+    StaticMeshComponent->SetVectorParameterValueOnMaterials(FName("Color"), ChangeColor());
+
+}
+
+void APickableActor::BeginPlay()
+{
+    Super::BeginPlay();
+
+}
+
+void APickableActor::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
--- Source/HM1/PickableActor.h
+++ Source/HM1/PickableActor.h
@@ -4,14 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "BaseActor.h"
+#include "ClickableInterface.h"
 #include "PickableActor.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class HM1_API APickableActor : public ABaseActor
+class HM1_API APickableActor : public ABaseActor, public IClickableInterface
 {
 	GENERATED_BODY()
 	
+public:
+
+	APickableActor();
+
+	void Click_Implementation();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 };
