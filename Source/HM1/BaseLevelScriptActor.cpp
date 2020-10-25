f978b315942191894424fc3f7238b0a969259270
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-25 16:22:00 +1000

    Fix Spawn SphereClickableActor in BaseLevelScriptActor

--- Source/HM1/BaseLevelScriptActor.cpp
+++ Source/HM1/BaseLevelScriptActor.cpp
@@ -9,9 +9,9 @@ void ABaseLevelScriptActor::BeginPlay()
 
     GetWorld()->SpawnActor<ASphereClickableActor>(ASphereClickableActor::StaticClass(), FVector(0.f, 0.f, 500.f), FRotator(0.f, 0.f, 0.f), FActorSpawnParameters());
     
-    for (int i = -500; i <= 500; i += 100)
+    for (int i = -500; i < 500; i += 100)
     {
-        for (int j = 50; j <= 1050; j += 100)
+        for (int j = 50; j < 1050; j += 100)
         {
             GetWorld()->SpawnActor<ACubeClickableActor>(ACubeClickableActor::StaticClass(), FVector(-1000.f, i, j), FRotator(0.f, 0.f, 0.f), FActorSpawnParameters());
         }
