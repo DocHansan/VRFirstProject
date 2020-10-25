1403550514d9e3c83877c3dabc979e0a9c97c9d5
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-25 16:48:49 +1000

    Create SpherePickableActor

---
+++ Source/HM1/SpherePickableActor.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickableActor.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Engine/StaticMesh.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "Materials/MaterialInstanceConstant.h"
+#include "SpherePickableActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HM1_API ASpherePickableActor : public APickableActor
+{
+	GENERATED_BODY()
+	
+
+public:
+
+	ASpherePickableActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UMaterialInstanceConstant* ConstantMaterialInst = nullptr;
+
+	UMaterialInstanceDynamic* DynamicMaterialInst = nullptr;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
