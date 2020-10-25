7d35f5922e2c03dc99a05fcc4db142b0729d3d33
Ilya Razumov <dangerous-hanter@mail.ru>
2020-10-25 21:56:41 +1000

    Fix spawn in BaseLevelScriptActor

--- Source/HM1/BaseLevelScriptActor.cpp
+++ Source/HM1/BaseLevelScriptActor.cpp
@@ -7,17 +7,17 @@ void ABaseLevelScriptActor::BeginPlay()
 {
     Super::BeginPlay();
 
-    for (int i = -500; i < 500; i += 100)
+    for (int i = -500; i < 600; i += 105)
     {
-        for (int j = 50; j < 1050; j += 100)
+        for (int j = 50; j < 1150; j += 100)
         {
             GetWorld()->SpawnActor<ACubeClickableActor>(ACubeClickableActor::StaticClass(), FVector(-1000.f, i, j), FRotator(0.f, 0.f, 0.f), FActorSpawnParameters());
         }
     }
 
-    for (int i = -500; i < 500; i += 100)
+    for (int i = -500; i < 600; i += 105)
     {
-        for (int j = 50; j < 1050; j += 100)
+        for (int j = 50; j < 1150; j += 100)
         {
             GetWorld()->SpawnActor<ACubePickableActor>(ACubePickableActor::StaticClass(), FVector(-3000.f, i, j), FRotator(0.f, 0.f, 0.f), FActorSpawnParameters());
         }
